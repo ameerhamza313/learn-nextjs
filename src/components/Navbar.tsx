7a4026b454962925d67f1bc2d419fd8bf9d852cb
ameerhamza313 <ameerhamzasanadi@gmail.com>
2024-10-18 10:11:26 +0530

    Added Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React, { useState } from "react";
+import { Menu, MenuItem,} from "@/components/ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+export function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Services">
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Products">
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Pricing">
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,7 +6,11 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -14,6 +19,18 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
